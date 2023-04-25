aa20a10385a691133dee48ee42a8ad96e98db359
PiotrK <piotrek380@gmail.com>
2023-04-25 22:47:49 +0200

    Add some test Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  return <h1>APP</h1>;
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </>
+  );
+}
+
+function HomePage() {
+  return <h1>Home</h1>;
+}
+
+function AboutPage() {
+  return <h1>About</h1>;
 }
 
 export default App;
